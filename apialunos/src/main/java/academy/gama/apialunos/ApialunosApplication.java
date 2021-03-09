b37f21ebaca77e6c554d0b563eec1d9cd69f96a8
GRenoldi <guilherme.renoldi@gmail.com>
2021-03-09 13:39:52 -0300

    Added the swagger documentation.

--- apialunos/src/main/java/academy/gama/apialunos/ApialunosApplication.java
+++ apialunos/src/main/java/academy/gama/apialunos/ApialunosApplication.java
@@ -2,8 +2,10 @@ package academy.gama.apialunos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ApialunosApplication {
 	public static void main(String[] args) {
 		 SpringApplication.run(ApialunosApplication.class, args);
